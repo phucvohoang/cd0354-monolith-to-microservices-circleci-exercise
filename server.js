49f8f6c96f8c5ba4a5b341bf6d48baba6f1316c7
phucvo <phucvoaff@gmail.com>
2023-05-06 16:55:20 +0700

    push example_nodebuild

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
 
+// This line added to check CI
 app.get('/', (req, res) => {
-  res.send('Horray! Your Express server is up and running.')
-})
+  res.send('Horray! Your Express server is up and running.');
+});
 
 app.listen(port, () => {
-  console.log(`Express app listening on port ${port}`)
-})
+  console.log(`Express app listening on port ${port}`);
+});
